5940ce56b17df5f526b1e4aafa55df38f0124a0c
777 <a@>
2025-08-23 20:06:13 +0300

    commited

--- WaveBox/models.py
+++ WaveBox/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Track(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    audiofile = models.FileField()
+
+    def __str__(self):
+        return self.name
--- WaveBoxV2/urls.py
+++ WaveBoxV2/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import re_path, path, include
 from django.views.static import serve
