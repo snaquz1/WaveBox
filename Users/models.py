f1ef0f3d1ab8739ac233578ea06ff72906fe46dc
777 <a@>
2025-10-16 06:17:46 +0300

    users

--- Users/models.py
+++ Users/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class CustomUser(AbstractUser):
     # Добавляем дополнительные поля
     username = models.CharField(max_length=20, unique=True)
-    avatar = models.ImageField(upload_to="images/user_avatars", null=True, blank=True)
+    avatar = models.ImageField(upload_to="images/user_avatars", default="images/user_avatars/noavatar.png")
+    bio = models.TextField(max_length=50, blank=True)
 
     def __str__(self):
         return self.username
